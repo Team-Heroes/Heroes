51b9065883478c5993d40357ef022362fd8cee0b
oluwamayowaf <e_fadairo@yahoo.com>
2019-09-15 23:49:05 +0100

    Retrieve Form data PHP

---
+++ formValidation.php
@@ -0,0 +1,13 @@
+<?php
+// Retrieve form data on submit 
+if ( isset( $_POST['submit'] ) ) {
+
+    $username = $_REQUEST['username'];
+    $password = $_REQUEST['password'];
+    $rememberme = $_REQUEST['rememberme'];
+
+}
+
+
+
+?>
